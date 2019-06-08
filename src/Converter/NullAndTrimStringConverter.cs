1553bf8a0ad7613e43d10eb93923d94ac81a879b
Stano Peťko <stano@stanopetko.eu>
2019-06-08 14:12:53 +0200

    NullAndTrimStringConverter

---
+++ src/Converter/NullAndTrimStringConverter.cs
@@ -0,0 +1,96 @@
+﻿using Kros.KORM.Properties;
+using System;
+
+namespace Kros.KORM.Converter
+{
+    /// <summary>
+    /// <para>
+    /// Converter for string values. Based on settings, it can convert <see langword="null"/> and <see cref="DBNull.Value"/>
+    /// values to empty string and trim string value.
+    /// </para>
+    /// <para>
+    /// The converter cannot be directly instantiated. Instead of that, there are predefined static instances
+    /// <see cref="ConvertNull"/>, <see cref="TrimString"/> and <see cref="ConvertNullAndTrimString"/>.
+    /// </para>
+    /// <para>
+    /// Converter can be used for example to ensure, that value going into the database will be empty string if the column
+    /// is set <c>NOT NULL</c>.
+    /// </para>
+    /// </summary>
+    public class NullAndTrimStringConverter : IConverter
+    {
+        /// <summary>
+        /// Instance of the converter with settings:
+        /// <see cref="ConvertNullValue"/> is <see langword="true"/>, <see cref="TrimStringValue"/> is <see langword="false"/>.
+        /// </summary>
+        public static readonly NullAndTrimStringConverter ConvertNull = new NullAndTrimStringConverter(true, false);
+
+        /// <summary>
+        /// Instance of the converter with settings:
+        /// <see cref="ConvertNullValue"/> is <see langword="false"/>, <see cref="TrimStringValue"/> is <see langword="true"/>.
+        /// </summary>
+        public static readonly NullAndTrimStringConverter TrimString = new NullAndTrimStringConverter(false, true);
+
+        /// <summary>
+        /// Instance of the converter with settings:
+        /// <see cref="ConvertNullValue"/> is <see langword="true"/>, <see cref="TrimStringValue"/> is <see langword="true"/>.
+        /// </summary>
+        public static readonly NullAndTrimStringConverter ConvertNullAndTrimString = new NullAndTrimStringConverter(true, true);
+
+        internal NullAndTrimStringConverter(bool convertNullValue, bool trimStringValue)
+        {
+            if ((convertNullValue == false) && (trimStringValue == false))
+            {
+                string msg = Resources.NullAndTrimStringConverter_InvalidArguments;
+                throw new ArgumentException(
+                    string.Format(msg, nameof(convertNullValue), nameof(trimStringValue)),
+                    nameof(convertNullValue) + ", " + nameof(trimStringValue));
+            }
+            ConvertNullValue = convertNullValue;
+            TrimStringValue = trimStringValue;
+        }
+
+        /// <summary>
+        /// If <see langword="true"/>, the values <see langword="null"/> and <see cref="DBNull"/> for database are converted
+        /// to empty string.
+        /// </summary>
+        public bool ConvertNullValue { get; }
+
+        /// <summary>
+        /// If <see langword="true"/> and input value for database is string, it is trimmed
+        /// (<see cref="string.Trim()">String.Trim</see>).
+        /// </summary>
+        public bool TrimStringValue { get; }
+
+        /// <inheritdoc cref="ConvertBack(object)"/>
+        public object Convert(object value) => ConvertValue(value);
+
+        /// <summary>
+        /// Converts <paramref name="value"/> based on <see cref="ConvertNullValue"/> and <see cref="TrimStringValue"/> settings.
+        /// </summary>
+        /// <param name="value">Value to convert.</param>
+        /// <returns>
+        /// <list type="bullet">
+        /// <item>If <see cref="ConvertNullValue"/> is <see langword="true"/> and the <paramref name="value"/> is
+        /// <see langword="null"/> or <see cref="DBNull"/>, empty string is returned.</item>
+        /// <item>If <paramref name="value"/> is string and <see cref="TrimStringValue"/> is <see langword="true"/>,
+        /// input value is trimmed and returned (<see cref="string.Trim()">String.Trim</see>).</item>
+        /// <item>In all other cases the <paramref name="value"/> is returned unchanged.</item>
+        /// </list>
+        /// </returns>
+        public object ConvertBack(object value) => ConvertValue(value);
+
+        private object ConvertValue(object value)
+        {
+            if ((value is null) || (value == DBNull.Value))
+            {
+                return ConvertNullValue ? string.Empty : value;
+            }
+            if (value is string strValue)
+            {
+                return TrimStringValue ? strValue.Trim() : strValue;
+            }
+            return value;
+        }
+    }
+}
---
+++ tests/Kros.KORM.UnitTests/Converter/NullAndTrimStringConverterShould.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using Kros.KORM.Converter;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Kros.KORM.UnitTests.Converter
+{
+    public class NullAndTrimStringConverterShould
+    {
+        [Fact]
+        public void NotCreateUselessInstance()
+        {
+            Action action = () => new NullAndTrimStringConverter(false, false);
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Theory]
+        [MemberData(nameof(DataConvertNullValuesIsTrue))]
+        public void ConvertValuesWhenConvertNullValuesIsTrue(object value, object expected, string because)
+        {
+            var converter = new NullAndTrimStringConverter(true, false);
+            converter.ConvertBack(value).Should().Be(expected, because);
+        }
+
+        public static IEnumerable<object[]> DataConvertNullValuesIsTrue()
+        {
+            yield return new object[] { null, string.Empty, "Input is null." };
+            yield return new object[] { DBNull.Value, string.Empty, "Input is DBNull.Value." };
+            yield return new object[] { "  \t ", "  \t ", "Input is whitespace string a trimming is off." };
+            yield return new object[] { "lorem ipsum", "lorem ipsum", "Input is string." };
+            yield return new object[] { 123, 123, "Input is not string." };
+        }
+
+        [Theory]
+        [MemberData(nameof(DataTrimStringValuesIsTrue))]
+        public void ConvertValuesWhenTrimStringValueIsTrue(object value, object expected, string because)
+        {
+            var converter = new NullAndTrimStringConverter(false, true);
+            converter.ConvertBack(value).Should().Be(expected, because);
+        }
+
+        public static IEnumerable<object[]> DataTrimStringValuesIsTrue()
+        {
+            yield return new object[] { null, null, "Input is null and null converting is off." };
+            yield return new object[] { DBNull.Value, DBNull.Value, "Input is DBNull.Value and null converting is off." };
+            yield return new object[] { "  \t ", string.Empty, "Input is whitespace string." };
+            yield return new object[] { " lorem ipsum \t", "lorem ipsum", "Input is string with whitespaces at the ends." };
+            yield return new object[] { "lorem ipsum", "lorem ipsum", "Input is string." };
+            yield return new object[] { 123, 123, "Input is not string." };
+        }
+
+        [Theory]
+        [MemberData(nameof(DataConvertNullValuesAndTrimStringValueAreTrue))]
+        public void ConvertValuesWhenConvertNullValuesAndTrimStringValueAreTrue(object value, object expected, string because)
+        {
+            var converter = new NullAndTrimStringConverter(true, true);
+            converter.ConvertBack(value).Should().Be(expected, because);
+        }
+
+        public static IEnumerable<object[]> DataConvertNullValuesAndTrimStringValueAreTrue()
+        {
+            yield return new object[] { null, string.Empty, "Input is null." };
+            yield return new object[] { DBNull.Value, string.Empty, "Input is DBNull.Value." };
+            yield return new object[] { "  \t ", string.Empty, "Input is whitespace string." };
+            yield return new object[] { " lorem ipsum \t", "lorem ipsum", "Input is string with whitespaces at the ends." };
+            yield return new object[] { "lorem ipsum", "lorem ipsum", "Input is string." };
+            yield return new object[] { 123, 123, "Input is not string." };
+        }
+    }
+}
