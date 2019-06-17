10983004caffb31efc099b948ad5db32b399e632
Stano Peťko <stano@stanopetko.eu>
2019-06-17 21:24:00 +0200

    Strings moved to resources

--- src/ThrowHelper.cs
+++ src/ThrowHelper.cs
@@ -1,27 +1,26 @@
 ﻿using Kros.KORM.Converter;
+using Kros.KORM.Properties;
 using System;
 
 namespace Kros.KORM
 {
     internal static class ThrowHelper
     {
-        // RES:
         public static void PropertyAlreadyConfigured(string propertyName)
-            => throw new InvalidOperationException(string.Format("Property \"{0}\" was already configured.", propertyName));
+            => throw new InvalidOperationException(string.Format(Resources.ThrowHelper_PropertyAlreadyConfigured, propertyName));
 
         public static void ColumnMappingAlreadyConfigured(string propertyName, string columnName, string currentMapping)
             => throw new InvalidOperationException(
-                string.Format("Trying to set column mapping \"{0}\" for property \"{1}\". Column mapping is already set as \"{2}\".",
-                columnName, propertyName, currentMapping));
+                string.Format(Resources.ThrowHelper_ColumnMappingAlreadyConfigured, columnName, propertyName, currentMapping));
 
         public static void ConverterAlreadyConfigured(string propertyName, IConverter converter, IConverter currentConverter)
             => throw new InvalidOperationException(
-                string.Format("Trying to set converter \"{0}\" for property \"{1}\". Converter is already set as \"{2}\".",
-                converter, propertyName, currentConverter.GetType().FullName));
+                string.Format(Resources.ThrowHelper_ConverterAlreadyConfigured,
+                converter.GetType().FullName, propertyName, currentConverter.GetType().FullName));
 
         public static void ConverterForTypeAlreadyConfigured(Type propertyType, IConverter converter, IConverter currentConverter)
             => throw new InvalidOperationException(
-                string.Format("Trying to set converter \"{0}\" for properties of type \"{1}\". Converter is already set as \"{2}\".",
+                string.Format(Resources.ThrowHelper_ConverterForTypeAlreadyConfigured,
                 converter.GetType().FullName, propertyType.Name, currentConverter.GetType().FullName));
     }
 }
