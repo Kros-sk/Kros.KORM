Kros-sk/Kros.KORM

commit 9f498ee1fb75068a09a23c7746c4e2e672e5bc1f

Removed dependency on static fields like databaseMapper, ... (#26)
* Removed dependency on static fields like databaseMapper, ...

* Update src/Database.DatabaseBuilder.cs

Co-Authored-By: Stano Peťko <stano@stanopetko.eu>

* Update src/Database.DatabaseBuilder.cs

Co-Authored-By: Stano Peťko <stano@stanopetko.eu>

* Update src/Database.DatabaseBuilder.cs

Co-Authored-By: Stano Peťko <stano@stanopetko.eu>

* Update src/Database.DatabaseBuilder.cs

Co-Authored-By: Stano Peťko <stano@stanopetko.eu>

src/Database.DatabaseBuilder.cs
@@ -1,0 +1,183 @@
+﻿using Kros.KORM.Materializer;
+using Kros.KORM.Metadata;
+using Kros.KORM.Query;
+using Kros.Utils;
+using System;
+using System.Configuration;
+using System.Data.Common;
+
+namespace Kros.KORM
+{
+    public partial class Database
+    {
+        private class DatabaseBuilder : IDatabaseBuilder
+        {
+            private IQueryProviderFactory _queryProviderFactory;
+            private ConnectionStringSettings _connectionString;
+            private DbConnection _connection;
+            private IModelFactory _modelFactory;
+            private DatabaseConfigurationBase _databaseConfiguration;
+            private readonly Lazy<ConventionModelMapper> _conventionModelMapper;
+            private readonly Lazy<DatabaseMapper> _databaseMapper;
+            private readonly Lazy<IQueryProvider> _queryProvider;
+            private readonly Lazy<ModelBuilder> _modelBuilder;
+            private bool _wasBuildCall = false;
+
+            public DatabaseBuilder()
+            {
+                _conventionModelMapper = new Lazy<ConventionModelMapper>(CreateModelMapper);
+                _databaseMapper = new Lazy<DatabaseMapper>(() => new DatabaseMapper(_conventionModelMapper.Value));
+                _queryProvider = new Lazy<IQueryProvider>(CreateQueryProvider);
+                _modelBuilder = new Lazy<ModelBuilder>(CreateModelBuilder);
+            }
+
+            private ModelBuilder CreateModelBuilder()
+            {
+                if (_modelFactory != null)
+                {
+                    return new ModelBuilder(_modelFactory);
+                }
+                else
+                {
+                    return new ModelBuilder(new DynamicMethodModelFactory(_databaseMapper.Value));
+                }
+            }
+
+            private IQueryProvider CreateQueryProvider()
+            {
+                IQueryProviderFactory factory;
+                if (_connectionString != null)
+                {
+                    factory = _queryProviderFactory ?? QueryProviderFactories.GetFactory(_connectionString.ProviderName);
+                    return factory.Create(_connectionString, _modelBuilder.Value, _databaseMapper.Value);
+                }
+                else
+                {
+                    factory = _queryProviderFactory ?? QueryProviderFactories.GetFactory(_connection);
+                    return factory.Create(_connection, _modelBuilder.Value, _databaseMapper.Value);
+                }
+            }
+
+            private ConventionModelMapper CreateModelMapper()
+            {
+                var modelMapper = new ConventionModelMapper();
+                if (_databaseConfiguration != null)
+                {
+                    var modelBuilder = new ModelConfigurationBuilder();
+                    _databaseConfiguration.OnModelCreating(modelBuilder);
+                    modelBuilder.Build(modelMapper);
+                }
+
+                return modelMapper;
+            }
+
+            public IDatabaseBuilder UseConnection(ConnectionStringSettings connectionString)
+            {
+                CheckDuplicateSettingForConnection();
+                CheckMultipleConfiguration();
+
+                _connectionString = Check.NotNull(connectionString, nameof(connectionString));
+
+                return this;
+            }
+
+            public IDatabaseBuilder UseConnection(string connectionString, string adoClientName)
+            {
+                CheckDuplicateSettingForConnection();
+                CheckMultipleConfiguration();
+
+                _connectionString = new ConnectionStringSettings(
+                    "KORM",
+                    Check.NotNullOrWhiteSpace(connectionString, nameof(connectionString)),
+                    Check.NotNullOrWhiteSpace(adoClientName, nameof(adoClientName)));
+
+                return this;
+            }
+
+            public IDatabaseBuilder UseConnection(DbConnection connection)
+            {
+                CheckDuplicateSettingForConnection();
+                CheckMultipleConfiguration();
+
+                _connection = Check.NotNull(connection, nameof(connection));
+
+                return this;
+            }
+
+            public IDatabaseBuilder UseQueryProviderFactory(IQueryProviderFactory queryProviderFactory)
+            {
+                CheckMultipleConfiguration();
+                _queryProviderFactory = Check.NotNull(queryProviderFactory, nameof(queryProviderFactory));
+
+                return this;
+            }
+
+            public IDatabaseBuilder UseModelFactory(IModelFactory modelFactory)
+            {
+                CheckMultipleConfiguration();
+                _modelFactory = Check.NotNull(modelFactory, nameof(modelFactory));
+
+                return this;
+            }
+
+            public IDatabaseBuilder UseDatabaseConfiguration<TConfiguration>()
+                where TConfiguration : DatabaseConfigurationBase, new()
+                => UseDatabaseConfiguration(new TConfiguration());
+
+            public IDatabaseBuilder UseDatabaseConfiguration(DatabaseConfigurationBase databaseConfiguration)
+            {
+                CheckMultipleConfiguration();
+
+                _databaseConfiguration = Check.NotNull(databaseConfiguration, nameof(databaseConfiguration));
+
+                return this;
+            }
+
+            public IDatabase Build()
+            {
+                CheckIfConnectionIsSet();
+                _wasBuildCall = true;
+
+                var database = new Database();
+
+                SetDatabaseMapper(database);
+                SetModelBuilder(database);
+                SetQueryProvider(database);
+
+                return database;
+            }
+
+            private void CheckDuplicateSettingForConnection()
+            {
+                if (_connectionString != null || _connection != null)
+                {
+                    throw new InvalidOperationException(
+                        string.Format(Properties.Resources.UseConnectionCanBeCallOnlyOne, nameof(UseConnection)));
+                }
+            }
+
+            private void CheckIfConnectionIsSet()
+            {
+                if (_connectionString is null && _connection is null)
+                {
+                    throw new InvalidOperationException(
+                        string.Format(Properties.Resources.UseConnectionMustBeCall, nameof(UseConnection), nameof(Build)));
+                }
+            }
+
+            private void CheckMultipleConfiguration()
+            {
+                if (_wasBuildCall)
+                {
+                    throw new InvalidOperationException(Properties.Resources.ConfigurationIsNotAllowed);
+                }
+            }
+
+            private void SetModelBuilder(Database database) => database._modelBuilder = _modelBuilder.Value;
+
+            private void SetQueryProvider(Database database) => database._queryProvider = _queryProvider.Value;
+
+            private void SetDatabaseMapper(Database database) => database._databaseMapper = _databaseMapper.Value;
+        }
+    }
+}

src/Database.cs
@@ -26,7 +26,7 @@ namespace Kros.KORM
     /// </para>
     /// </summary>
     /// <seealso cref="IModelBuilder" />
-    public class Database : IDatabase
+    public partial class Database : IDatabase
     {
         #region Static
 
@@ -294,142 +294,6 @@ namespace Kros.KORM
             using (Kros.Data.IIdGenerator idGenerator = _queryProvider.CreateIdGenerator("DummyTableName", 1))
             {
                 idGenerator.InitDatabaseForIdGenerator();
-            }
-        }
-
-        #endregion
-
-        #region IDatabaseBuilder
-
-        private class DatabaseBuilder : IDatabaseBuilder
-        {
-            private IQueryProviderFactory _queryProviderFactory;
-            private ConnectionStringSettings _connectionString;
-            private DbConnection _connection;
-            private IModelFactory _modelFactory;
-            private DatabaseConfigurationBase _databaseConfiguration;
-
-            public IDatabaseBuilder UseConnection(ConnectionStringSettings connectionString)
-            {
-                CheckDuplicateSettingForConnection();
-                _connectionString = Check.NotNull(connectionString, nameof(connectionString));
-
-                return this;
-            }
-
-            public IDatabaseBuilder UseConnection(string connectionString, string adoClientName)
-            {
-                CheckDuplicateSettingForConnection();
-                _connectionString = new ConnectionStringSettings(
-                    "KORM",
-                    Check.NotNullOrWhiteSpace(connectionString, nameof(connectionString)),
-                    Check.NotNullOrWhiteSpace(adoClientName, nameof(adoClientName)));
-
-                return this;
-            }
-
-            public IDatabaseBuilder UseConnection(DbConnection connection)
-            {
-                CheckDuplicateSettingForConnection();
-                _connection = Check.NotNull(connection, nameof(connection));
-
-                return this;
-            }
-
-            public IDatabaseBuilder UseQueryProviderFactory(IQueryProviderFactory queryProviderFactory)
-            {
-                _queryProviderFactory = Check.NotNull(queryProviderFactory, nameof(queryProviderFactory));
-
-                return this;
-            }
-
-            public IDatabaseBuilder UseModelFactory(IModelFactory modelFactory)
-            {
-                _modelFactory = Check.NotNull(modelFactory, nameof(modelFactory));
-
-                return this;
-            }
-
-            public IDatabaseBuilder UseDatabaseConfiguration<TConfiguration>()
-                where TConfiguration : DatabaseConfigurationBase, new()
-                => UseDatabaseConfiguration(new TConfiguration());
-
-            public IDatabaseBuilder UseDatabaseConfiguration(DatabaseConfigurationBase databaseConfiguration)
-            {
-                _databaseConfiguration = Check.NotNull(databaseConfiguration, nameof(databaseConfiguration));
-
-                return this;
-            }
-
-            public IDatabase Build()
-            {
-                CheckIfConnectionIsSet();
-
-                var database = new Database();
-
-                BuildModelMapper();
-                SetDatabaseMapper(database);
-                SetModelBuilder(database);
-                SetQueryProvider(database);
-
-                return database;
-            }
-
-            private void CheckDuplicateSettingForConnection()
-            {
-                if (_connectionString != null || _connection != null)
-                {
-                    throw new InvalidOperationException(
-                        string.Format(Properties.Resources.UseConnectionCanBeCallOnlyOne, nameof(UseConnection)));
-                }
-            }
-
-            private void CheckIfConnectionIsSet()
-            {
-                if (_connectionString is null && _connection is null)
-                {
-                    throw new InvalidOperationException(
-                        string.Format(Properties.Resources.UseConnectionMustBeCall, nameof(UseConnection), nameof(Build)));
-                }
-            }
-
-            private void SetModelBuilder(Database database)
-            {
-                if (_modelFactory != null)
-                {
-                    database._modelBuilder = new ModelBuilder(_modelFactory);
-                }
-                else
-                {
-                    database._modelBuilder = new ModelBuilder(new DynamicMethodModelFactory(Database.DatabaseMapper));
-                }
-            }
-
-            private void SetQueryProvider(Database database)
-            {
-                IQueryProviderFactory factory;
-                if (_connectionString != null)
-                {
-                    factory = _queryProviderFactory ?? QueryProviderFactories.GetFactory(_connectionString.ProviderName);
-                    database._queryProvider = factory.Create(_connectionString, database.ModelBuilder, Database.DatabaseMapper);
-                }
-                else
-                {
-                    factory = _queryProviderFactory ?? QueryProviderFactories.GetFactory(_connection);
-                    database._queryProvider = factory.Create(_connection, database.ModelBuilder, Database.DatabaseMapper);
-                }
-            }
-
-            private void SetDatabaseMapper(Database database) => database._databaseMapper = Database.DatabaseMapper;
-
-            private void BuildModelMapper()
-            {
-                if (_databaseConfiguration != null)
-                {
-                    var modelBuilder = new ModelConfigurationBuilder();
-                    _databaseConfiguration.OnModelCreating(modelBuilder);
-                    modelBuilder.Build(DefaultModelMapper as ConventionModelMapper);
-                }
             }
         }
 

src/Properties/Resources.Designer.cs
@@ -142,6 +142,15 @@ namespace Kros.KORM.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to The configuration is not allowed if the Build method has already been called..
+        /// </summary>
+        internal static string ConfigurationIsNotAllowed {
+            get {
+                return ResourceManager.GetString("ConfigurationIsNotAllowed", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to The constant for &apos;{0}&apos; is not supported..
         /// </summary>
         internal static string ConstantIsNotSupported {

src/Properties/Resources.resx
@@ -146,6 +146,9 @@
   <data name="CompositePrimaryKeyMustHaveSameNameInAllColumns" xml:space="preserve">
     <value>Primary key of the model is composite. The name of the key must be the same for all its columns.</value>
   </data>
+  <data name="ConfigurationIsNotAllowed" xml:space="preserve">
+    <value>The configuration is not allowed if the Build method has already been called.</value>
+  </data>
   <data name="ConstantIsNotSupported" xml:space="preserve">
     <value>The constant for '{0}' is not supported.</value>
   </data>

tests/Kros.KORM.UnitTests/DatabaseBuilderShould.cs
@@ -126,7 +126,7 @@ $@"CREATE TABLE [dbo].[Foo] (
             DatabaseShouldNotBeNull(database);
 
             database.Query<Foo>().FirstOrDefault(f => f.Id == 1);
-            queryProviderFactory.Received().Create(ServerHelper.Connection, database.ModelBuilder, Database.DatabaseMapper);
+            queryProviderFactory.Received().Create(ServerHelper.Connection, database.ModelBuilder, Arg.Any<DatabaseMapper>());
         }
 
         [Fact]
@@ -143,7 +143,7 @@ $@"CREATE TABLE [dbo].[Foo] (
             DatabaseShouldNotBeNull(database);
 
             database.Query<Foo>().FirstOrDefault(f => f.Id == 1);
-            queryProviderFactory.Received().Create(Arg.Any<ConnectionStringSettings>(), database.ModelBuilder, Database.DatabaseMapper);
+            queryProviderFactory.Received().Create(Arg.Any<ConnectionStringSettings>(), database.ModelBuilder, Arg.Any<DatabaseMapper>());
         }
 
         [Fact]
@@ -197,6 +197,56 @@ $@"CREATE TABLE [dbo].[Foo] (
             database.Query<Bar>()
                 .FirstOrDefault(b => b.RowId == 1)
                 .Age.Should().Be(11);
+        }
+
+        [Fact]
+        public void BuildInstanceMoreTimes()
+        {
+            var builder = Database
+                .Builder
+                .UseConnection(ServerHelper.Connection)
+                .UseDatabaseConfiguration<DatabaseConfiguration>();
+
+            IDatabase database = builder.Build();
+            database.Query<Bar>()
+                .FirstOrDefault(b => b.RowId == 1)
+                .Age.Should().Be(11);
+
+            IDatabase database2 = builder.Build();
+            database2.Query<Bar>()
+                .FirstOrDefault(b => b.RowId == 1)
+                .Age.Should().Be(11);
+        }
+
+        [Fact]
+        public void ThrowExceptionWhenTryConfigureAfterBuild()
+        {
+            IDatabaseBuilder builder = Database
+                .Builder
+                .UseConnection(ServerHelper.Connection);
+
+            builder.Build();
+
+            IModelFactory modelFactory = Substitute.For<IModelFactory>();
+            IQueryProviderFactory queryProviderFactory = Substitute.For<IQueryProviderFactory>();
+
+            void ShouldThrowException(Action action)
+            {
+                action
+                    .Should()
+                    .Throw<InvalidOperationException>(
+                    "The configuration is not allowed if the Build method has already been called.");
+            }
+
+            ShouldThrowException(() => builder.UseConnection(ServerHelper.Connection));
+            ShouldThrowException(() => builder.UseConnection(
+                new ConnectionStringSettings("KORM", ServerHelper.Connection.ConnectionString, "System.Data.SqlClient")));
+            ShouldThrowException(() => builder.UseConnection(
+                ServerHelper.Connection.ConnectionString, "System.Data.SqlClient"));
+            ShouldThrowException(() => builder.UseDatabaseConfiguration<DatabaseConfiguration>());
+            ShouldThrowException(() => builder.UseDatabaseConfiguration(new DatabaseConfiguration()));
+            ShouldThrowException(() => builder.UseModelFactory(modelFactory));
+            ShouldThrowException(() => builder.UseQueryProviderFactory(queryProviderFactory));
         }
 
         private static void DatabaseShouldNotBeNull(IDatabase database)

commit 3f4dccb855dc17de1e51a97f48e6fc4041d58bfd

DatabaseBuilder creates a new QueryProvider for every new database (#27)
Commit https://github.com/Kros-sk/Kros.KORM/commit/9f498ee1fb75068a09a23c7746c4e2e672e5bc1f introduced a bug. DatabaseBuilder used the same QueryProvider for all built databases. This is fix for it - a new instance of QueryProvider needs to be created for new database.

src/Database.DatabaseBuilder.cs
@@ -1,4 +1,4 @@
-﻿using Kros.KORM.Materializer;
+using Kros.KORM.Materializer;
 using Kros.KORM.Metadata;
 using Kros.KORM.Query;
 using Kros.Utils;
@@ -19,7 +19,6 @@ namespace Kros.KORM
             private DatabaseConfigurationBase _databaseConfiguration;
             private readonly Lazy<ConventionModelMapper> _conventionModelMapper;
             private readonly Lazy<DatabaseMapper> _databaseMapper;
-            private readonly Lazy<IQueryProvider> _queryProvider;
             private readonly Lazy<ModelBuilder> _modelBuilder;
             private bool _wasBuildCall = false;
 
@@ -27,7 +26,6 @@ namespace Kros.KORM
             {
                 _conventionModelMapper = new Lazy<ConventionModelMapper>(CreateModelMapper);
                 _databaseMapper = new Lazy<DatabaseMapper>(() => new DatabaseMapper(_conventionModelMapper.Value));
-                _queryProvider = new Lazy<IQueryProvider>(CreateQueryProvider);
                 _modelBuilder = new Lazy<ModelBuilder>(CreateModelBuilder);
             }
 
@@ -175,7 +173,7 @@ namespace Kros.KORM
 
             private void SetModelBuilder(Database database) => database._modelBuilder = _modelBuilder.Value;
 
-            private void SetQueryProvider(Database database) => database._queryProvider = _queryProvider.Value;
+            private void SetQueryProvider(Database database) => database._queryProvider = CreateQueryProvider();
 
             private void SetDatabaseMapper(Database database) => database._databaseMapper = _databaseMapper.Value;
         }

src/Kros.KORM.csproj
@@ -2,7 +2,7 @@
   <PropertyGroup>
     <TargetFrameworks>netstandard2.0;netcoreapp2.1;net46</TargetFrameworks>
     <Company>KROS a.s.</Company>
-    <Version>4.0.0-alfa4</Version>
+    <Version>4.0.0-alfa5</Version>
     <Authors>KROS a.s.</Authors>
     <Title>Kros.KORM</Title>
     <Description>KORM is fast, easy to use, micro .NET Standard ORM tool. (Kros Object Relation Mapper)</Description>