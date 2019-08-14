b7d60cc2d335de604a087bcd8f169c27fe75f02e
Lukas Sefcik <lukas.sefcik@gmail.com>
2019-08-14 20:23:25 +0200

    Extracted class to file.

---
+++ src/CurrentTimeValueGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Kros.KORM
+{
+    /// <summary>
+    /// Current time value generator.
+    /// </summary>
+    /// <remarks>
+    /// Generator generates date and time that are set to the current Coordinated Universal Time (UTC).
+    /// </remarks>
+    public class CurrentTimeValueGenerator : IValueGenerator
+    {
+        /// <inheritdoc />
+        public object GetValue() => DateTimeOffset.UtcNow;
+    }
+}
--- src/IValueGenerator.cs
+++ src/IValueGenerator.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Kros.KORM
+﻿namespace Kros.KORM
 {
     /// <summary>
     /// Interface for column value generator.
@@ -11,17 +9,5 @@ namespace Kros.KORM
         /// Gets value.
         /// </summary>
         object GetValue();
-    }
-
-    /// <summary>
-    /// Current time value generator.
-    /// </summary>
-    /// <remarks>
-    /// Generator generates date and time that are set to the current Coordinated Universal Time (UTC).
-    /// </remarks>
-    public class CurrentTimeValueGenerator : IValueGenerator
-    {
-        /// <inheritdoc />
-        public object GetValue() => DateTimeOffset.UtcNow;
     }
 }
