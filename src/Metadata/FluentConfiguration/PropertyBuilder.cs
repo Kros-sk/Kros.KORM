9ee3b8dfcb2ed29d90ab24dedc02dffda3053a46
Lukas Sefcik <lukas.sefcik@gmail.com>
2019-08-14 20:25:55 +0200

    Extracted PropertyBuilderExtensions to file.

--- src/Metadata/FluentConfiguration/PropertyBuilder.cs
+++ src/Metadata/FluentConfiguration/PropertyBuilder.cs
@@ -103,26 +103,4 @@ namespace Kros.KORM.Metadata
             return _entityTypeBuilder;
         }
     }
-
-    internal static class PropertyBuilderExtensions
-    {
-        internal static IEntityTypePropertyBuilder<TEntity> UseCurrentTimeValueGenerator<TEntity>(
-            this IPropertyBuilder<TEntity> propertyBuilder, ValueGenerated valueGenerated) where TEntity : class
-        {
-            switch (valueGenerated)
-            {
-                case ValueGenerated.OnInsert:
-                    return propertyBuilder.UseValueGeneratorOnInsert(new CurrentTimeValueGenerator());
-
-                case ValueGenerated.OnUpdate:
-                    return propertyBuilder.UseValueGeneratorOnUpdate(new CurrentTimeValueGenerator());
-
-                case ValueGenerated.OnInsertOrUpdate:
-                    return propertyBuilder.UseValueGeneratorOnInsertOrUpdate(new CurrentTimeValueGenerator());
-            }
-
-            throw new NotSupportedException(
-                string.Format(Resources.ValueGeneratedNeverNotSupported, nameof(CurrentTimeValueGenerator)));
-        }
-    }
 }
---
+++ src/Metadata/FluentConfiguration/PropertyBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Kros.KORM.Properties;
+using System;
+
+namespace Kros.KORM.Metadata
+{
+    internal static class PropertyBuilderExtensions
+    {
+        internal static IEntityTypePropertyBuilder<TEntity> UseCurrentTimeValueGenerator<TEntity>(
+            this IPropertyBuilder<TEntity> propertyBuilder, ValueGenerated valueGenerated) where TEntity : class
+        {
+            switch (valueGenerated)
+            {
+                case ValueGenerated.OnInsert:
+                    return propertyBuilder.UseValueGeneratorOnInsert(new CurrentTimeValueGenerator());
+
+                case ValueGenerated.OnUpdate:
+                    return propertyBuilder.UseValueGeneratorOnUpdate(new CurrentTimeValueGenerator());
+
+                case ValueGenerated.OnInsertOrUpdate:
+                    return propertyBuilder.UseValueGeneratorOnInsertOrUpdate(new CurrentTimeValueGenerator());
+            }
+
+            throw new NotSupportedException(
+                string.Format(Resources.ValueGeneratedNeverNotSupported, nameof(CurrentTimeValueGenerator)));
+        }
+    }
+}
