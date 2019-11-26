af7eb689876874de1a1e204f57a3ca29a79f49e9
Jakub Berthoty <berthotyms@kros.sk>
2019-11-26 10:09:38 +0100

    Added support for entities collection insert/update/delete and entities collection bulk insert/update to database extensions.

--- src/IDatabaseExtensions.cs
+++ src/IDatabaseExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Kros.KORM.Query;
 using Kros.KORM.Query.Sql;
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
@@ -21,6 +22,26 @@ namespace Kros.KORM
             => await CommitChangesAsync(database, (IDbSet<TEntity> dbSet) => dbSet.Add(entity));
 
         /// <summary>
+        /// Adds <paramref name="entities"/> to the database.
+        /// </summary>
+        /// <typeparam name="TEntity">Entities type.</typeparam>
+        /// <param name="database"><see cref="IDatabase"/> instance.</param>
+        /// <param name="entities">Entities to add.</param>
+        public static async Task AddAsync<TEntity>(this IDatabase database, IEnumerable<TEntity> entities) where TEntity : class
+            => await CommitChangesAsync(database, (IDbSet<TEntity> dbSet) => dbSet.Add(entities));
+
+        /// <summary>
+        /// Adds <paramref name="entities"/> to the database via bulk insert.
+        /// </summary>
+        /// <typeparam name="TEntity">Entities type.</typeparam>
+        /// <param name="database"><see cref="IDatabase"/> instance.</param>
+        /// <param name="entities">Entities to add.</param>
+        public static async Task AddBulkAsync<TEntity>(
+            this IDatabase database,
+            IEnumerable<TEntity> entities) where TEntity : class
+            => await ProcessBulkOperationAsync(database, async (IDbSet<TEntity> dbSet) => await dbSet.BulkInsertAsync(entities));
+
+        /// <summary>
         /// Deletes the <paramref name="entity"/> from the database.
         /// </summary>
         /// <typeparam name="TEntity">Entity type.</typeparam>
@@ -28,6 +49,17 @@ namespace Kros.KORM
         /// <param name="entity">The entity to delete.</param>
         public static async Task DeleteAsync<TEntity>(this IDatabase database, TEntity entity) where TEntity : class
             => await CommitChangesAsync(database, (IDbSet<TEntity> dbSet) => dbSet.Delete(entity));
+
+        /// <summary>
+        /// Deletes <paramref name="entities"/> from the database.
+        /// </summary>
+        /// <typeparam name="TEntity">Entities type.</typeparam>
+        /// <param name="database"><see cref="IDatabase"/> instance.</param>
+        /// <param name="entities">Entities to delete.</param>
+        public static async Task DeleteAsync<TEntity>(
+            this IDatabase database,
+            IEnumerable<TEntity> entities) where TEntity : class
+            => await CommitChangesAsync(database, (IDbSet<TEntity> dbSet) => dbSet.Delete(entities));
 
         /// <summary>
         /// Deletes the <typeparamref name="TEntity"/> with <paramref name="id"/> from the database.
@@ -72,6 +104,26 @@ namespace Kros.KORM
             => await CommitChangesAsync(database, (IDbSet<TEntity> dbSet) => dbSet.Edit(entity));
 
         /// <summary>
+        /// Edits <paramref name="entities"/> in the database.
+        /// </summary>
+        /// <typeparam name="TEntity">Entities type.</typeparam>
+        /// <param name="database"><see cref="IDatabase"/> instance.</param>
+        /// <param name="entities">Entities to edit.</param>
+        public static async Task EditAsync<TEntity>(this IDatabase database, IEnumerable<TEntity> entities) where TEntity : class
+            => await CommitChangesAsync(database, (IDbSet<TEntity> dbSet) => dbSet.Edit(entities));
+
+        /// <summary>
+        /// Edits <paramref name="entities"/> in the database via bulk update.
+        /// </summary>
+        /// <typeparam name="TEntity">Entities type.</typeparam>
+        /// <param name="database"><see cref="IDatabase"/> instance.</param>
+        /// <param name="entities">Entities to edit.</param>
+        public static async Task EditBulkAsync<TEntity>(
+            this IDatabase database,
+            IEnumerable<TEntity> entities) where TEntity : class
+            => await ProcessBulkOperationAsync(database, async (IDbSet<TEntity> dbSet) => await dbSet.BulkUpdateAsync(entities));
+
+        /// <summary>
         /// Edits the <paramref name="entity"/> in the database.
         /// </summary>
         /// <typeparam name="TEntity">Entity type.</typeparam>
@@ -84,18 +136,30 @@ namespace Kros.KORM
             params string[] columns) where TEntity : class
             => await CommitChangesAsync(database, (IDbSet<TEntity> dbSet) => dbSet.Edit(entity), columns);
 
+        private static IDbSet<TEntity> GetDbSet<TEntity>(IDatabase database, params string[] columns) where TEntity : class
+            => columns.Length == 0
+                ? database.Query<TEntity>().AsDbSet()
+                : database.Query<TEntity>().Select(columns).AsDbSet();
+
         private static async Task CommitChangesAsync<TEntity>(
             IDatabase database,
             Action<IDbSet<TEntity>> action,
             params string[] columns) where TEntity : class
         {
-            IDbSet<TEntity> dbSet = columns.Length == 0
-                ? database.Query<TEntity>().AsDbSet()
-                : database.Query<TEntity>().Select(columns).AsDbSet();
+            IDbSet<TEntity> dbSet = GetDbSet<TEntity>(database, columns);
 
             action(dbSet);
 
             await dbSet.CommitChangesAsync();
+        }
+
+        private static async Task ProcessBulkOperationAsync<TEntity>(
+            IDatabase database,
+            Func<IDbSet<TEntity>, Task> action,
+            params string[] columns) where TEntity : class
+        {
+            IDbSet<TEntity> dbSet = GetDbSet<TEntity>(database, columns);
+            await action(dbSet);
         }
     }
 }
