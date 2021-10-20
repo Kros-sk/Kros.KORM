ef0c168b461a63e644a9307cb870c8df48c4fdd6
Ján Slivka <slivka@kros.sk>
2021-10-20 08:19:09 +0200

    Execute with temp table extension with object definition.

--- tests/Kros.KORM.UnitTests/Integration/IDatabaseExtensionsTempTableShould.cs
+++ tests/Kros.KORM.UnitTests/Integration/IDatabaseExtensionsTempTableShould.cs
@@ -1,6 +1,9 @@
 ﻿using FluentAssertions;
 using Kros.Extensions;
+using Kros.KORM.Metadata.Attribute;
 using Kros.KORM.UnitTests.Base;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -41,6 +44,72 @@ namespace Kros.KORM.UnitTests.Integration
         }
 
         [Fact]
+        public void ExecuteWithTempTableObjectList()
+        {
+            using (IDatabase database = CreateDatabase(CreateTable_TestTable, InsertDataScript2))
+            {
+                List<IdDto> ids = CreateIds();
+                var affectedCount = database.ExecuteWithTempTable(
+                    ids,
+                    (database, tableName) => database.ExecuteNonQuery(
+                        $@"UPDATE P
+                          SET P.Age = 18
+                          FROM People AS P INNER JOIN {tableName} AS T ON (P.Id = T.Id)"));
+
+                affectedCount.Should().Be(4);
+            }
+        }
+
+        [Fact]
+        public void ExecuteWithTempTableObjectListNoMapColumnThrowException()
+        {
+            using (IDatabase database = CreateDatabase(CreateTable_TestTable, InsertDataScript2))
+            {
+                List<TestDto> data = CreateTestData();
+                Action act = () => database.ExecuteWithTempTable(
+                    data,
+                    (database, tableName) => database.ExecuteNonQuery(
+                        $@"UPDATE P
+                          SET P.Age = 18
+                          FROM People AS P INNER JOIN {tableName} AS T ON (P.Id = T.Number)"));
+                act.Should().Throw<SqlException>().WithMessage("Invalid column name 'Number'.");
+            }
+        }
+
+        [Fact]
+        public void ExecuteWithTempTableObjectListAliasColumnThrowException()
+        {
+            using (IDatabase database = CreateDatabase(CreateTable_TestTable, InsertDataScript2))
+            {
+                List<TestDto> data = CreateTestData();
+                Action act = () => database.ExecuteWithTempTable(
+                    data,
+                    (database, tableName) => database.ExecuteNonQuery(
+                        $@"UPDATE P
+                          SET P.Age = 18
+                          FROM People AS P INNER JOIN {tableName} AS T ON (P.Id = T.Value)"));
+                act.Should().Throw<SqlException>().WithMessage("Invalid column name 'Value'.");
+            }
+        }
+
+        [Fact]
+        public void ExecuteWithTempTableObjectListAliasColumn()
+        {
+            using (IDatabase database = CreateDatabase(CreateTable_TestTable, InsertDataScript2))
+            {
+                List<TestDto> data = CreateTestData();
+                var affectedCount = database.ExecuteWithTempTable(
+                    data,
+                    (database, tableName) => database.ExecuteNonQuery(
+                        $@"UPDATE P
+                          SET P.Age = 18
+                          FROM People AS P INNER JOIN {tableName} AS T ON (P.Id = T.Id)"));
+
+                affectedCount.Should().Be(1);
+            }
+        }
+
+        [Fact]
         public async Task ExecuteWithTempTableListAsync()
         {
             using (IDatabase database = CreateDatabase(CreateTable_TestTable, InsertDataScript2))
@@ -52,6 +121,23 @@ namespace Kros.KORM.UnitTests.Integration
                         $@"UPDATE P
                           SET P.Age = 18
                           FROM People AS P INNER JOIN {tableName} AS T ON (P.Id = T.Value)"));
+
+                affectedCount.Should().Be(4);
+            }
+        }
+
+        [Fact]
+        public async Task ExecuteWithTempTableObjectListAsync()
+        {
+            using (IDatabase database = CreateDatabase(CreateTable_TestTable, InsertDataScript2))
+            {
+                List<IdDto> ids = CreateIds();
+                var affectedCount = await database.ExecuteWithTempTableAsync(
+                    ids,
+                    (database, tableName) => database.ExecuteNonQueryAsync(
+                        $@"UPDATE P
+                          SET P.Age = 18
+                          FROM People AS P INNER JOIN {tableName} AS T ON (P.Id = T.Id)"));
 
                 affectedCount.Should().Be(4);
             }
@@ -73,6 +159,23 @@ namespace Kros.KORM.UnitTests.Integration
         }
 
         [Fact]
+        public void ExecuteWithTempTableTObjectList()
+        {
+            using (IDatabase database = CreateDatabase(CreateTable_TestTable, InsertDataScript2))
+            {
+                List<IdDto> ids = CreateIds();
+                IEnumerable<Person> result = database.ExecuteWithTempTable(
+                    ids,
+                    (database, tableName) => database.Query<Person>()
+                    .Select("P.*")
+                    .From($"People AS P INNER JOIN {tableName} AS T ON (P.Id = T.Id)")
+                    .ToList());
+
+                result.Should().HaveCount(4);
+            }
+        }
+
+        [Fact]
         public async Task ExecuteWithTempTableTListAsync()
         {
             using (IDatabase database = CreateDatabase(CreateTable_TestTable, InsertDataScript2))
@@ -82,6 +185,24 @@ namespace Kros.KORM.UnitTests.Integration
                     ids,
                     (database, tableName) => database.Query<Person>()
                     .From($"People AS P INNER JOIN {tableName} AS T ON (P.Id = T.Value)")
+                    .ToList()
+                    .AsTask());
+
+                result.Should().HaveCount(4);
+            }
+        }
+
+        [Fact]
+        public async Task ExecuteWithTempTableTObjectListAsync()
+        {
+            using (IDatabase database = CreateDatabase(CreateTable_TestTable, InsertDataScript2))
+            {
+                List<IdDto> ids = CreateIds();
+                IEnumerable<Person> result = await database.ExecuteWithTempTableAsync(
+                    ids,
+                    (database, tableName) => database.Query<Person>()
+                    .Select("P.*")
+                    .From($"People AS P INNER JOIN {tableName} AS T ON (P.Id = T.Id)")
                     .ToList()
                     .AsTask());
 
@@ -174,6 +295,41 @@ namespace Kros.KORM.UnitTests.Integration
 
                 result.Should().HaveCount(2);
             }
+        }
+
+        private static List<IdDto> CreateIds()
+            => new List<IdDto>()
+            {
+                new IdDto(1),
+                new IdDto(2),
+                new IdDto(3),
+                new IdDto(4),
+                new IdDto(456),
+                new IdDto(789)
+            };
+
+        private static List<TestDto> CreateTestData()
+            => new List<TestDto>()
+            {
+                new TestDto(1,1),
+                new TestDto(789,789)
+            };
+
+        private record IdDto(int Id);
+
+        private class TestDto
+        {
+            public TestDto(int id, int number)
+            {
+                Id = id;
+                Number = number;
+            }
+
+            [Alias("Value")]
+            public int Id { get; set; }
+
+            [NoMap]
+            public int Number { get; set; }
         }
     }
 }
