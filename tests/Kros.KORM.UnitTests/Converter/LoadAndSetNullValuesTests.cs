228e983e8120eabba5ebc45bf6e3e01df0704668
Stano Peťko <stano@stanopetko.eu>
2022-04-15 17:42:45 +0200

    Add no database test

--- tests/Kros.KORM.UnitTests/Converter/LoadAndSetNullValuesTests.cs
+++ tests/Kros.KORM.UnitTests/Converter/LoadAndSetNullValuesTests.cs
@@ -1,19 +1,26 @@
 ﻿using FluentAssertions;
 using Kros.KORM.Converter;
+using Kros.KORM.Materializer;
 using Kros.KORM.Metadata;
 using Kros.KORM.UnitTests.Base;
 using Newtonsoft.Json;
+using NSubstitute;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using Xunit;
 
 namespace Kros.KORM.UnitTests.Converter
 {
     public class LoadAndSetNullValuesTests : DatabaseTestBase
     {
+        #region Database tests
+
         #region Database schema
 
-        private const string TableName = "WeatherForecast";
+        private const string TableName = "DbNullValues";
 
         private static readonly string CreateTableScript =
 $@"CREATE TABLE [dbo].[{TableName}] (
@@ -90,6 +97,23 @@ INSERT INTO [{TableName}] VALUES (12, NULL);";
 
         #endregion
 
+        [Theory]
+        [MemberData(nameof(SetNullValueFromDatabase_Data))]
+        public void SetNullValueFromDatabase(int id, string expectedData)
+        {
+            using TestDatabase testDb = CreateDatabase(new[] { CreateTableScript, InsertDataScript });
+            using IDatabase db = CreateDatabaseWithConfiguration(testDb);
+
+            DataItemWithDefaultValue expectedItem = new()
+            {
+                Id = id,
+                Data = expectedData
+            };
+            DataItemWithDefaultValue actualItem = db.Query<DataItemWithDefaultValue>().Where(item => item.Id == id).Single();
+
+            actualItem.Should().BeEquivalentTo(expectedItem);
+        }
+
         public static IEnumerable<object[]> UseConverterForNullValue_Data()
         {
             yield return new object[] { 1, new List<string>() };
@@ -123,21 +147,154 @@ INSERT INTO [{TableName}] VALUES (12, NULL);";
             yield return new object[] { 12, null };
         }
 
-        [Theory]
-        [MemberData(nameof(SetNullValueFromDatabase_Data))]
-        public void SetNullValueFromDatabase(int id, string expectedData)
+        #endregion
+
+        #region No database tests
+
+        private class PropInfo
         {
-            using TestDatabase testDb = CreateDatabase(new[] { CreateTableScript, InsertDataScript });
-            using IDatabase db = CreateDatabaseWithConfiguration(testDb);
-
-            DataItemWithDefaultValue expectedItem = new()
-            {
-                Id = id,
-                Data = expectedData
-            };
-            DataItemWithDefaultValue actualItem = db.Query<DataItemWithDefaultValue>().Where(item => item.Id == id).Single();
-
-            actualItem.Should().BeEquivalentTo(expectedItem);
+            public string Name { get; set; }
+            public Type PropertyType { get; set; }
         }
+
+        private class DataInfo<T>
+        {
+            private readonly Dictionary<string, int> _nameMap = new();
+            private readonly Dictionary<int, PropInfo> _info = new();
+            private readonly IDatabaseMapper _databaseMapper;
+            private readonly IDataReader _dataReader;
+
+            public DataInfo()
+            {
+                Type dataType = typeof(T);
+                PropertyInfo[] props = dataType.GetProperties();
+                int i = 0;
+                foreach (PropertyInfo prop in props)
+                {
+                    _info.Add(i, new PropInfo()
+                    {
+                        Name = prop.Name,
+                        PropertyType = prop.PropertyType
+                    });
+                    _nameMap.Add(prop.Name, i);
+                    i++;
+                }
+                _databaseMapper = CreateDatabaseMapper();
+                _dataReader = CreateDataReader();
+            }
+
+            public int Count => _info.Count;
+            public Type GetFieldType(int i) => _info[i].PropertyType;
+            public string GetFieldName(int i) => _info[i].Name;
+            public IDatabaseMapper DatabaseMapper => _databaseMapper;
+            public IDataReader DataReader => _dataReader;
+
+            private static IDatabaseMapper CreateDatabaseMapper()
+            {
+                IDatabaseMapper mapper = Substitute.For<IDatabaseMapper>();
+
+                ConventionModelMapper modelMapper = new();
+                TableInfo tableInfo = modelMapper.GetTableInfo<TestItem>();
+                mapper.GetTableInfo<T>().Returns(tableInfo);
+                mapper.GetTableInfo(typeof(T)).Returns(tableInfo);
+
+                return mapper;
+            }
+
+            private IDataReader CreateDataReader()
+            {
+                IDataReader reader = Substitute.For<IDataReader>();
+
+                reader.FieldCount.Returns(Count);
+                reader.GetFieldType(Arg.Any<int>()).Returns(ci => _info[ci.Arg<int>()].PropertyType);
+                reader.GetName(Arg.Any<int>()).Returns(ci => _info[ci.Arg<int>()].Name);
+                reader.IsDBNull(Arg.Any<int>()).Returns(ci =>
+                {
+                    Console.WriteLine("IDataReader.IsDBNull");
+                    return true;
+                });
+
+                return reader;
+            }
+        }
+
+        private class TestItem
+        {
+            public TestItem() : this(true)
+            {
+            }
+
+            public TestItem(bool initValuesInConstructor)
+            {
+                if (initValuesInConstructor)
+                {
+                    StrVal = "Lorem";
+                    BoolVal = true;
+                    ByteVal = 1;
+                    SByteVal = 2;
+                    Int16Val = 3;
+                    UInt16Val = 4;
+                    Int32Val = 5;
+                    UInt32Val = 6;
+                    Int64Val = 7;
+                    UInt64Val = 8;
+                    CharVal = 'a';
+                    DoubleVal = 3.14;
+                    SingleVal = 6.28F;
+                    NullableBoolVal = true;
+                    NullableByteVal = 1;
+                    NullableSByteVal = 2;
+                    NullableInt16Val = 3;
+                    NullableUInt16Val = 4;
+                    NullableInt32Val = 5;
+                    NullableUInt32Val = 6;
+                    NullableInt64Val = 7;
+                    NullableUInt64Val = 8;
+                    NullableCharVal = 'a';
+                    NullableDoubleVal = 3.14;
+                    NullableSingleVal = 6.28F;
+                }
+            }
+
+            public string StrVal { get; set; }
+            public bool BoolVal { get; set; }
+            public byte ByteVal { get; set; }
+            public sbyte SByteVal { get; set; }
+            public short Int16Val { get; set; }
+            public ushort UInt16Val { get; set; }
+            public int Int32Val { get; set; }
+            public uint UInt32Val { get; set; }
+            public long Int64Val { get; set; }
+            public ulong UInt64Val { get; set; }
+            public char CharVal { get; set; }
+            public double DoubleVal { get; set; }
+            public float SingleVal { get; set; }
+            public bool? NullableBoolVal { get; set; }
+            public byte? NullableByteVal { get; set; }
+            public sbyte? NullableSByteVal { get; set; }
+            public short? NullableInt16Val { get; set; }
+            public ushort? NullableUInt16Val { get; set; }
+            public int? NullableInt32Val { get; set; }
+            public uint? NullableUInt32Val { get; set; }
+            public long? NullableInt64Val { get; set; }
+            public ulong? NullableUInt64Val { get; set; }
+            public char? NullableCharVal { get; set; }
+            public double? NullableDoubleVal { get; set; }
+            public float? NullableSingleVal { get; set; }
+        }
+
+        [Fact]
+        public void Test()
+        {
+            DataInfo<TestItem> info = new();
+            DynamicMethodModelFactory modelFactory = new DynamicMethodModelFactory(info.DatabaseMapper);
+            Func<IDataReader, TestItem> factory = modelFactory.GetFactory<TestItem>(info.DataReader);
+
+            TestItem actual = factory(info.DataReader);
+            TestItem expected = new(false);
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+#endregion
     }
 }
